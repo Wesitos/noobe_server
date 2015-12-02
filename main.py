616ccf2390afe37e78eea32b534f15274ccdf857
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2015-12-02 03:00:01 -0500

    Arreglado orden de queries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tornado.options import define, options
 import tornado.escape
 import datetime
 from motor import MotorClient
+from pymongo import ASCENDING,DESCENDING
 from bson.objectid import ObjectId
 from parse import parse_message
 from tcp_server import TCPSocketHandler
@@ -41,13 +42,16 @@ def insert_data(data_dict, db=_db_object,
 
 @gen.coroutine
 def get_data(query, limit, from_store=False,
+             tailable=False,
                   db=_db_object,
                   store_coll=options["store-coll"],
                   capped_coll=options["capped-coll"]):
     if from_store:
-        return db[store_coll].find(query).limit(limit)
-    else:
+        return db[store_coll].find(query, sort=[("$natural", DESCENDING)]).limit(limit)
+    elif tailable:
         return db[capped_coll].find(query, tailable=True).limit(limit)
+    else:
+        return db[capped_coll].find(query, sort=[("$natural", DESCENDING)]).limit(limit)
 
 
 class DataHandler(tornado.web.RequestHandler):
@@ -66,25 +70,42 @@ class DataHandler(tornado.web.RequestHandler):
         """
         historic = (self.get_argument("historic", None) is not None)
         after = self.get_argument("after", None)
+        before = self.get_argument("before", None)
         count = int(self.get_argument("count", 100) )
 
-        query = {"_id":{"$gt": ObjectId(after)}} if after else {}
+        match =  {k:ObjectId(val) for k,val in [("$gt", after), ("lt", before)] if val}
+
+        query = {"_id": {"$elemenMatch": match}} if match else {}
 
         cursor = yield get_data(query, count, historic)
         data = []
-        last_id = ""
+        last_id = None
+        first_id  = None 
+        # Tomamos todo lo que podamos
+        while (yield cursor.fetch_next):
+            item = cursor.next_object()
+            # Removemos el _id, pero antes, lo almacenamos
+            _id = item.pop("_id")
+            first_id = first_id or _id
+            last_id = _id
+            data.append(item)
+
         # Debemos devolver aunque sea un dato
-        while (len(data) == 0 and cursor.alive):
-            # Tomamos todo lo que podamos
-            while (yield cursor.fetch_next):
-                item = cursor.next_object()
-                # Removemos el _id, pero antes, lo almacenamos
-                last_id = item.pop("_id")
-                data.append(item)
+        if(len(data) == 0):
+            cursor = yield get_data(query, count, historic, True)
+            while (len(data) == 0 and cursor.alive):
+                while (yield cursor.fetch_next):
+                    item = cursor.next_object()
+                    # Removemos el _id, pero antes, lo almacenamos
+                    _id = item.pop("_id")
+                    first_id = first_id or _id
+                    last_id = _id
+                    data.append(item)
 
         self.write({
             "data": data,
             "last": str(last_id),
+            "first": str(first_id),
         })
 
 
